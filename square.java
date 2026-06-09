1362026c3dfa872db2c329c273be45a2e9558ab6
pranishkarki20 <praniskkarki20@gmail.com>
2026-06-09 21:22:20 +0530

    patterns2

---
+++ square.java
@@ -0,0 +1,11 @@
+public class square {
+  public static void main (String[]  args){
+    for ( int i = 0 ; i < 4 ; i ++){
+        for (int j = 0 ; j < 4 ; j++){
+            System.err.print("*");
+        }
+        System.out.println();
+    }
+  }  
+}
+  
